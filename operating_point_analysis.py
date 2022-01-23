87de9d349d322f05f0418d66ed13ee56bfda28d1
Sabrina Chowdhury <sabrina.chowdhury29@gmail.com>
2022-01-23 17:45:45 +0000

    update

---
+++ operating_point_analysis.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+import PySpice
+import PySpice.Logging.Logging as Logging
+from PySpice.Spice.Netlist import Circuit
+from PySpice.Unit import *
