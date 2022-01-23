46726e2530e558fa8c041fb635b62ee25a740046
Sabrina Chowdhury <sabrina.chowdhury29@gmail.com>
2022-01-23 18:28:15 +0000

    update

--- simple_voltage_divider/simple_voltage_divider.py
+++ simple_voltage_divider/simple_voltage_divider.py
@@ -19,19 +19,25 @@ circuit.V('input', 'in', circuit.gnd, 10@u_V)
 circuit.R(1, 'in', 'out', 9@u_kOhm)
 circuit.R(2, 'out', circuit.gnd, 1@u_kOhm)
 
-# # print the circuit:
-# print("\nThe Circuit/Netlist: \n", circuit)
+# print the circuit:
+print("\nThe Circuit/Netlist: \n", circuit)
 
 # create a simulator object (with parameters e.g temp)
 simulator = circuit.simulator(temperature=25, nominal_temperature=25)
 
 # print the circuit + simulator details:
-print("\nThe Simulator: \n", simulator)
+print("The Simulator: \n", simulator)
+
+# run analysis
+analysis = simulator.operating_point()
+
+# print the result
+print(analysis)
 
 exit()
 
 
-print(analysis)
+
 print(analysis.nodes['in'])
 print(float(analysis.nodes['in']))
 
