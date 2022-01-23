5f5f35cc4d6483c9257bebf60d64da7a18bf6108
Sabrina Chowdhury <sabrina.chowdhury29@gmail.com>
2022-01-23 21:08:16 +0000

    update

--- circuit_with_diode/circuit_with_diode.py
+++ circuit_with_diode/circuit_with_diode.py
@@ -39,14 +39,28 @@ def format_output(analysis):
 ############################################################
 
 # create the circuit
-circuit = Circuit('My Circuit')
+circuit = Circuit('Circuit with a Diode')
 
 # define the 1N4148PH (signal diode)
-circuit.model('MyDiode', 'D', IS=4.352@u_nA, RS=0.6458@u_Ohm, BV=110@u_V), IBV=0.0001@u_V, N=1.906)
+circuit.model('MyDiode', 'D', IS=4.352@u_nA, RS=0.6458@u_Ohm, BV=110@u_V, IBV=0.0001@u_V, N=1.906)
 
 # add components to the circuit
 circuit.V('input', 1, circuit.gnd, 10@u_V)
 circuit.R(1, 1, 2, 9@u_kOhm)
-circuit.R(2, 3, circuit.gnd, 9@u_kOhm)
+circuit.R(2, 3, circuit.gnd, 1@u_kOhm)
 circuit.Diode(1, 2, 3, model='MyDiode')
 circuit.Diode(2, 3, circuit.gnd, model='MyDiode')
+
+# print the circuit:
+print("\nThe Circuit/Netlist: \n", circuit)
+
+# # create a simulator object (with parameters e.g temp)
+# simulator = circuit.simulator(temperature=25, nominal_temperature=25)
+
+# # print the circuit + simulator details:
+# print("The Simulator: \n", simulator)
+
+# # run analysis
+# analysis = simulator.operating_point()
+# out_dict = format_output(analysis)
+# print(out_dict)
