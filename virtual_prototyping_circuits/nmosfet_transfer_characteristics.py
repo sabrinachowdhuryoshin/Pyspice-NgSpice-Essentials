499c836ebb34be1581a2c20f46a3b96084c2b7d5
Sabrina Chowdhury <sabrina.chowdhury29@gmail.com>
2022-01-25 13:21:40 +0000

    update

--- virtual_prototyping_circuits/nmosfet_transfer_characteristics.py
+++ virtual_prototyping_circuits/nmosfet_transfer_characteristics.py
@@ -1,9 +1,7 @@
 import matplotlib.pyplot as plt
 
-
 import PySpice.Logging.Logging as Logging
 logger = Logging.setup_logging()
-
 
 from PySpice.Doc.ExampleTools import find_libraries
 from PySpice.Probe.Plot import plot
@@ -11,13 +9,12 @@ from PySpice.Spice.Library import SpiceLibrary
 from PySpice.Spice.Netlist import Circuit
 from PySpice.Unit import *
 
-
 libraries_path = find_libraries()
 spice_library = SpiceLibrary(libraries_path)
 
-
-
-
+############################################################
+# # Make a Circuit with nmosfet # #
+############################################################
 
 circuit = Circuit('NMOS Transistor')
 circuit.include(spice_library['BUK7S1R0-40H'])
@@ -31,13 +28,14 @@ Vdrain = circuit.V('drain', 'vdd', circuit.gnd, u_V(Vdd))
 # M <name> <drain node> <gate node> <source node> <bulk/substrate node>
 circuit.MOSFET(1, 'vdd', 'gatenode', circuit.gnd, circuit.gnd, model='ptm65nm_nmos')
 
-
-
+# create a simulator object (with parameters e.g temp)
 simulator = circuit.simulator(temperature=25, nominal_temperature=25)
+
+# run DC sweep analysis
 analysis = simulator.dc(Vgate=slice(0, Vdd, .01))
 
+# plot graph
 figure, ax = plt.subplots(figsize=(20, 10))
-
 ax.plot(analysis['gatenode'], u_mA(-analysis.Vdrain))
 ax.legend('NMOS characteristic')
 ax.grid()
